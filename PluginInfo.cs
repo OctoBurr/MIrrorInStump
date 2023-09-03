c29c832c27326f0456b5b5967075747422f7c43d
Rascos <132259285+Rascos@users.noreply.github.com>
2023-09-03 17:17:33 +0100

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace StumpMirror
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.rascosgt.gorillatag.stumpmirror";
+        public const string Name = "StumpMirror";
+        public const string Version = "1.0.0";
+    }
+}
